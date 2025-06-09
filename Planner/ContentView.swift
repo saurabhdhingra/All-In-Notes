c90680182fbe8f8ea86eee1257e9dbdead036af7
saurabhZethic <saurabh@zethic.com>
2025-06-09 16:29:15 +0530

    Initial Commit

---
+++ Planner/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Planner
+//
+//  Created by Saurabh Dhingra on 09/06/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Planner/PlannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlannerApp.swift
+//  Planner
+//
+//  Created by Saurabh Dhingra on 09/06/25.
+//
+
+import SwiftUI
+
+@main
+struct PlannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
